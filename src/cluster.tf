031aa5fb90866072676fa348ef1cea9c1ca829df
yoshi <78339917+y-matsutomo@users.noreply.github.com>
2022-02-25 21:00:34 +0900

    Ensure your security group blocks unwanted outbound traffic This pull request improves our infrastructure by fixing a security issue found by [Shisho Cloud](https://shisho.dev).
    ## Description from Shisho Cloud
    
    Your security group allows egress traffic from all IPs, exposing resources in the VPC to unwanted attacks. If it is intentional, you can help your colleagues by leaving a comment that says it is intentionally done so.

--- src/cluster.tf
+++ src/cluster.tf
@@ -42,10 +42,12 @@ resource "aws_security_group" "tfgoat-cluster" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"            
+    # [Shisho]: remove `0.0.0.0/0` from the following line and add appropriate IP ranges
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
+
 
 
 resource "aws_security_group_rule" "tfgoat-cluster-ingress-workstation-https" {
